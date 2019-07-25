8ec7b2bbca76659dbc4793f5cf5737dcd3c42e4f
OkCat <48531104+BeratKARATAS53@users.noreply.github.com>
2019-07-25 09:42:52 +0300

    Project Start

---
+++ src/main/java/com/obss/movieTracker/MovieTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.obss.movieTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTrackerApplication.class, args);
+	}
+
+}
